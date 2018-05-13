17922afb9cf01b557c10f7850d5024a2c3c303f7
Tharindu_Dhananjaya <tharindudhananjaya1122@gmail.com>
2018-05-13 23:55:57 +0530

    Update admin_home.php

--- admin_home.php
+++ admin_home.php
@@ -29,14 +29,14 @@ $DBcon->close();
    </header>
          <input type="button"  accesskey="1" value="Add User" onclick="window.location.href='###'" />
          <input type="button" accesskey="2" value="User Details" onclick="window.location.href='user_details.php'" />
-<input type="button" accesskey="3" value="Update User" onclick="window.location.href='update_user_find.php'" />
-<input type="button" accesskey="4" value="Delete User" onclick="window.location.href='delete_user_find.php'" />
-<input type="button" accesskey="5" value="Reset passwords" onclick="window.location.href='reset_password.php'" />
-<input type="button"  accesskey="6" value="View Customer" onclick="window.location.href='###'" />
-</div></center>
-<footer>
-<input type="button" accesskey="b" value="Back" onclick="window.location.href='###'" />
- </footer>      
-</form> 
+         <input type="button" accesskey="3" value="Update User" onclick="window.location.href='update_user_find.php'" />
+         <input type="button" accesskey="4" value="Delete User" onclick="window.location.href='delete_user_find.php'" />
+         <input type="button" accesskey="5" value="Reset passwords" onclick="window.location.href='reset_password.php'" />
+         <input type="button"  accesskey="6" value="View Customer" onclick="window.location.href='###'" />
+   </div></center>
+      <footer>
+         <input type="button" accesskey="b" value="Back" onclick="window.location.href='###'" />
+      </footer>      
+   </form> 
 </body>
 </html>
